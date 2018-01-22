3b577a6edf68f1e6cd8fa474eb150c09d5995087
Cecilia Wong <cissi.wong@gmail.com>
2018-01-22 11:19:00 +0100

    added backend dependencies

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,29 @@
 import express from "express"
+import mongoose from "mongoose"
+import bodyParser from "body-parser"
+import cors from "cors"
+import bcrypt from "bcrypt-nodejs"
 
 const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+
+// Tells express to add the "Access-Control-Allow-Origin" header to allow requests from anywhere.
+app.use(cors())
+
+
+// Connect to MongoDB, on the "products-api" database. If the db doesn't
+// exist, mongo will create it.
+mongoose.connect("mongodb://localhost/signup-form-api", { useMongoClient: true })
+
+// This makes mongo use ES6 promises, instead of its own implementation
+mongoose.Promise = Promise
+
+// Log when mongo connects, or encounters errors when trying to connect.
+mongoose.connection.on("error", err => console.error("Connection error:", err))
+mongoose.connection.once("open", () => console.log("Connected to mongodb"))
+
 
 app.get("/", (req, res) =>
   res.send("Hello World!")
