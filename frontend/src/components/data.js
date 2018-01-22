adfa5433138ecab41051eb50510581979cb953d1
Cecilia Wong <cissi.wong@gmail.com>
2018-01-22 13:36:10 +0100

    added data.js

---
+++ frontend/src/components/data.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    title: "Skrämd eller hotad",
+    question: "Har du känt dig skrämd eller hotad av något vårdpersonal sagt eller gjort mot dig under förlossningen? Ex. höjt rösten mot eller skällt på dig",
+    score: 1
+  },
+  {
+    title: "Personlig integritet",
+    question: "Har du erfarit att vårdpersonalen under din förlossning inte särskilt beaktat din personliga integritet? Ex för din nakenhet.",
+    score: 1
+  },
+  {
+    title: "Personligt utrymme",
+    question: "Har du erfarit att vårdpersonalen under din förlossning tagit på någon del av din kropp eller inkräktat på ditt personal space utan ditt tillstånd eller uttryckta godkännande? Ex stått för nära, tagit i hand, öppnat toadörren utan att knacka...",
+    score: 1
+  },
+  {
+    title: "Privata områden",
+    question: "Har du erfarit att någon ur vårdpersonalen någon gång under din förlossning tagit på dina privata områden utan ditt tillstånd? Privata områden är munnen, snippan, rektum och bröst.",
+    score: 2
+  },
+  {
+    title: "Tydligt våld",
+    question: "Har vårdpersonalen utsatt dig för tydligt våld under förlossningen? Ex på tydligt våld är fasthållning, bryska tag eller yttre press.",
+    score: 2
+  },
+  {
+    title: "Grovt våld",
+    question: "Har vårdpersonalen utsatt dig för grovt våld under förlossningen? Ex på grovt våld är slag, knuff, örfilar eller skakningar.",
+    score: 3
+  },
+  {
+    title: "Grovt våld",
+    question: "Har du erfarit att någon ur vårdpersonalen någon gång under din förlossning orsakat dig smärta som varit obefogad, oväntad, oförberedd eller utan förvarning eller utan ditt tillstånd?",
+    score: 3
+  },
+  {
+    title: "Särskilt grovt våld",
+    question: "Har du erfarit att någon ur vårdpersonalen någon gång under din förlossning penetrerat dina privata områden utan ditt tillstånd? Privata områden är munnen, snippan och rektum.",
+    score: 3
+  }
+]
